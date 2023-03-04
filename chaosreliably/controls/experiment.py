48d9d32a035ea81c25844a9ac742dbfc7769433d
Sylvain Hellegouarch <sh@defuze.org>
2023-03-04 15:47:34 +0100

    older version don't support list as type
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- chaosreliably/controls/experiment.py
+++ chaosreliably/controls/experiment.py
@@ -44,7 +44,7 @@ class ReliablyHandler(RunEventHandler):  # type: ignore
         self.started = datetime.utcnow().replace(tzinfo=timezone.utc)
         self.experiment = deepcopy(experiment)
         self.journal = None  # type: Journal
-        self.current_activities = []  # type: list[Activity]
+        self.current_activities = []  # type: ignore
 
         self.should_stop = threading.Event()
         self.should_pause = threading.Event()
